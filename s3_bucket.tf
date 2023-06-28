1b7f13278e9f6b52f9b4044aa97dec738c0ceca2
Owen Wu <55355014+owenwu811@users.noreply.github.com>
2023-06-27 20:13:50 -0700

    Update s3_bucket.tf

--- s3_bucket.tf
+++ s3_bucket.tf
@@ -1,3 +1,21 @@
 resource "aws_s3_bucket" "bucket1" {
-	bucket = "${var.app_region}-${var.bucket}"
+  bucket = "${var.app_region}-${var.bucket}"
+  
+  lifecycle_rule {
+    id      = "log-files"
+    status  = "enabled"
+    
+    transition {
+      days          = 30
+      storage_class = "GLACIER"
+    }
+    
+    expiration {
+      days = 365
+    }
+  }
+  
+  versioning {
+    enabled = true
+  }
 }
