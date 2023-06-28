be09ed641e5d69014b4e5d0fb8f3d679ce71a42f
Owen Wu <55355014+owenwu811@users.noreply.github.com>
2023-06-27 20:11:31 -0700

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -8,3 +8,13 @@ variable "app_region" {
 	type = string
 	default = "us-west-1"
 }
+
+variable "employee_count" {
+  type    = number
+  default = 10
+}
+
+variable "payroll_schedule" {
+  type    = string
+  default = "monthly"
+}
