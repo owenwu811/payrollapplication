4d8b27a5e17d01ec58d5db9999bc0c3db4a99f4d
Owen Wu <55355014+owenwu811@users.noreply.github.com>
2023-08-14 15:03:46 -0700

    Update modules.tf

--- modules.tf
+++ modules.tf
@@ -1,5 +1,5 @@
 module "uk_payroll" {
 	source = "../payroll-app" #child module we are calling from 
-	app_region = "us-west-1"
-	ami = "ami-00569e54da628d17c"
+	app_region = var.app_region
+	ami = var.ami
 }
